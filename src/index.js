d9ca92add1d500c13d583dcec515d5f6c95608b3
Ezat Elzalouy <ezatelzalouy711@gmail.com>
2020-02-09 12:50:53 +0200

    do changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import * as serviceWorker from "./serviceWorker";
-import "@fortawesome/fontawesome-free/css/all.css";
-console.log(process.env);
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
